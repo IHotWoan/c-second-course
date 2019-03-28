ffd72e668c507e38af98f60eddd1c471d54b7bb1
IHotWoan <felixrhodin@hotmail.com>
2019-03-28 14:35:58 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    cout << "Hello, World!" << endl;
+    int test = 20;
+    int test2 = 3;
+    cout << test + test2 << endl;
+    return 0;
+}
